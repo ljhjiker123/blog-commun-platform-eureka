ae9b69ef43ff1600881a6fe4822050d204fc2aa3
ljhjiker123 <810758134@qq.com>
2020-04-01 14:52:42 +0800

    111

---
+++ blog-commun-platform-webapi1/src/test/java/com/jiker/bcp/eureka/BlogCommunPlatformWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiker.bcp.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCommunPlatformWebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
